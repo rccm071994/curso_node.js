f419fb74d04b1e84a7f9d08010e47c115bc05dd6
rccmcota <rccm071994@gmail.com>
2018-01-09 22:59:23 -0400

    Cambiando MitoCode por Cota

--- basico/lodash_yargs/app.js
+++ basico/lodash_yargs/app.js
@@ -3,9 +3,9 @@ const argv = require('yargs').argv;
 
 //let comando = process.argv[2];
 
-if (argv.usuario === 'MitoCode') {    
+if (argv.usuario === 'Cota') {    
         let x = { "nombre": "Jaime" }
-        let y = { "apodo": "MitoCode" }
+        let y = { "apodo": "Cota" }
         let z = [
             { nombre: "Jaime", apellido: "Medina", edad: 26 },
             { nombre: "Mito", apellido: "Code", edad: 26 }
